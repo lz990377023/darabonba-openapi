5bbc2e7628527c0886cb2036e5564348c86df903
Trees <990377023@qq.com>
2025-02-27 10:31:23 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             .package(url: "https://github.com/aliyun/tea-swift.git", from: "1.0.2"),
             .package(url: "https://github.com/alibabacloud-sdk-swift/tea-utils", from: "1.0.6"),
             .package(url: "https://github.com/aliyun/credentials-swift", from: "1.0.1"),
-            .package(url: "https://github.com/alibabacloud-sdk-swift/openapi-util", from: "1.0.1"),
+            .package(url: "https://github.com/lz990377023/openapi-util.git", from: "1.0.2"),
             .package(url: "https://github.com/alibabacloud-sdk-swift/alibabacloud-gateway-spi", from: "0.0.1"),
             .package(url: "https://github.com/alibabacloud-sdk-swift/tea-xml", from: "1.0.1"),
         ],
